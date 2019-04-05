5048077d06965017108b5665b4748cce52191c39
serv <olivier.metzinger@epitech.eu>
2019-04-05 17:01:38 +0200

    add, modif, del ldap

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ modlist = {
     "uid": ["moreno"],
     "sn": ["cosani"],
     "givenName": ["Moreno"],
-    "cn": ["Moreno COSANI"],  ##MODIFIER LES STRINGS COMME BON VOUS SEMBLE
+    "cn": ["Moreno COSANI"],                                                                ##MODIFIER LES STRINGS COMME BON VOUS SEMBLE (set en global)
     "displayName": ["Moreno Cosani"],
     "userPassword": ["root"],
     "uidNumber": ["5000"],
@@ -23,9 +23,13 @@ modlist = {
 def add_user(load):
     global modlist
 
-    dn = "uid=moreno,ou=Personnes,dc=roederer,dc=fr"
-    load.add_s(dn, ldap.modlist.addModlist(modlist))
-
+    dn = "uid=moreno,ou=Personnes,dc=roederer,dc=fr"                                            #MODIFIER LE DN (chemin)
+    try:
+        load.add_s(dn, ldap.modlist.addModlist(modlist))
+        print("Utilisateur added")
+    except:
+        print("Erreur, verifiez votre dn, et controler que son uid n'existe pas deja")
+        return 84
 
 
 ########## modifier user ##########
@@ -34,19 +38,35 @@ def modif_user(load):
     global modlist
 
     dn = "uid=moreno,ou=Personnes,dc=roederer,dc=fr"
-    old_value = {"userPassword": ["VIEUX MDP"]}
-    new_value = {"userPassword": ["ANCIEN MDP"]}
+    try:
+        old_value = {"userPassword": ["ANCIEN MDP"]}                                            #MODIFIER LA CLASS ET TOUJOURS METTRE ANCIENNE VALEUR, PUIS NOUVELLE
+        new_value = {"userPassword": ["NOUVEAU MDP"]}
+        modlist = ldap.modlist.modifyModlist(old_value, new_value)
+        load.modify_s(dn, modlist)
+        print("Valeur modifiee")
+    except:
+        print("Erreur, verifiez votre dn, et controler que l'ancienne valeur est bonne")
+        return 84
 
-    modlist = ldap.modlist.modifyModlist(old_value, new_value)
-    load.modify_s(dn, modlist)
 
+
+########## supprimer user (par defaut) ##########
+
+def delete_user(load):
+    try:
+        dn = "uid=Olivier,ou=Personnes,dc=roederer,dc=fr"                                              #MODIFIER LE DN
+        load.delete_s(dn)
+        print("Supprime avez succes!")
+    except:
+        print("Erreur, avez-vous entre le bon DN ?")
+        return 84
 
 
 def main():
     try:
         load = ldap.initialize("ldap://127.0.0.1")
         load.simple_bind_s("cn=admin,dc=roederer,dc=fr", "root")                             #utilisation du compte admin pour effectuer les modifs
-        add_user(load)                                                                            #MODIFIER L'APPEL FONCTION POUR CHOISIR LA DEMARCHE A EFFECTUER
+        delete_user(load)                                                                     #MODIFIER L'APPEL FONCTION POUR CHOISIR LA DEMARCHE A EFFECTUER
     except ldap.LDAPError as e:
         print (e)
 
